92180a7ebe17c00cfba1d3e317ebe3614a7ed74d
darkevo2424 <timotius.albert@luxpmsoft.com>
2023-03-03 07:45:41 +0700

    updated material ui

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
 import SearchResults from '../components/resultsContainer';
 import React,{useRef} from 'react';
 import { CircularProgress } from "@material-ui/core";
